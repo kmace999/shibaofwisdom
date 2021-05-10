c0fdf14191bcc5f2a8c00856d35b5a35112567b0
kmace999 <kmace999@gmail.com>
2021-05-10 12:35:38 -0400

    redirected shiba images to separate folder, made advice returning function

--- app/shiba_app.py
+++ app/shiba_app.py
@@ -9,33 +9,17 @@ import shutil
 
 #advice code
 
-def advice_search(pointer):
-#don't forget a docstring
-    global parsed_response
-    global advicestr
-    searching = True
-    while searching:
-        if pointer == "random":
-            request_url = "https://api.adviceslip.com/advice"
-            searching = False
-        elif pointer == "ask":
-            advicestr = input("What do you wish to ask the Shiba of Wisdom for advice on?:")
-            #https://api.adviceslip.com/advice/search/spiders
-            if advicestr == "random":
-                request_url = "https://api.adviceslip.com/advice"
-                searching = False
-            else:
-                request_url = f"https://api.adviceslip.com/advice/search/{advicestr}"
 
-        response = requests.get(request_url)
-        parsed_response = json.loads(response.text)
-
-        if 'message' in parsed_response:
-            print(f"The Shiba of Wisdom cannot offer advice about {advicestr}. Please try again.")
-        else:
-            searching = False
-
-
+def get_advice(amount):
+#don't forget docstring
+    global your_advice
+    if amount > 1:
+        #love
+        rand_advice = random.randint(0,amount-1)
+        your_advice = parsed_response["slips"][rand_advice]['advice']
+    else:
+        #avocado
+        your_advice = parsed_response["slips"][0]["advice"]
 
 print("Welcome to Shiba of Wisdom!")
 checking = True
@@ -47,28 +31,33 @@ while checking:
     else:
         checking = False
 
-advice_search(advice_type)
+searching = True
+while searching:
+    if advice_type == "random":
+        request_url = "https://api.adviceslip.com/advice"
+        searching = False
+    elif advice_type == "ask":
+        advicestr = input("What do you wish to ask the Shiba of Wisdom for advice on?:")
+        #https://api.adviceslip.com/advice/search/spiders
+        if advicestr == "random":
+            request_url = "https://api.adviceslip.com/advice"
+            searching = False
+        else:
+            request_url = f"https://api.adviceslip.com/advice/search/{advicestr}"
+
+    response = requests.get(request_url)
+    parsed_response = json.loads(response.text)
+
+    if 'message' in parsed_response:
+        print(f"The Shiba of Wisdom cannot offer advice about {advicestr}. Please try again.")
+    else:
+        searching = False
 
 if advice_type == 'random':
     your_advice = parsed_response['slip']['advice']
 
-else:
-    advice_num = int(parsed_response["total_results"])
-    if advice_num > 1:
-        #love
-        rand_advice = random.randint(0,advice_num-1)
-        your_advice = parsed_response["slips"][rand_advice]['advice']
-    else:
-        #avocado
-        your_advice = parsed_response["slips"][0]["advice"]
-
-
-
-print(your_advice)
-print("- The Shiba of Wisdom")
-print("")
-print("You can view your Shiba of Wisdom in the shibas folder in this directory.")
-#maybe add extra part here to keep track of all your historical advice?
+advice_num = int(parsed_response["total_results"])
+get_advice(advice_num)
 
 #shiba image code
 
@@ -81,19 +70,20 @@ if advice_type == 'random':
 else:
     shibafilename = advicestr+ "_"+shibalink.split("/")[-1]
 
+shiba_pic_filepath = os.path.join(os.path.dirname(__file__), "..", "Shiba Images", shibafilename)
 
 #GETTING SHIBA IMAGE FROM REQUESTS
 picresponse = requests.get(shibalink, stream=True)
 
 
-#writing advice on shiba
-#play around with size
-#advice_font = ImageFont.truetype('FONT ADDRESS',200)
-
-
-
 #SAVING SHIBA IMAGE
 #save in separate folder than final shibas of wisdom
 picresponse.raw.decode_content = True
-with open(shibafilename,'wb') as f:
+with open(shiba_pic_filepath,'wb') as f:
     shutil.copyfileobj(picresponse.raw,f)
+
+print("")
+print(your_advice)
+print("- The Shiba of Wisdom")
+print("")
+print("You can view your Shiba of Wisdom in the Shiba Images folder in this directory.")
